5988cf2003a584d585d0c027d4d995faa2e6cd89
Le-Vrai-Krnivor <krnivor@proton.me>
2025-01-04 21:06:52 +0100

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,32 @@
 function toggleMenu() {
-    const navbar = document.getElementById('navbar');
-    navbar.style.display = navbar.style.display === 'block' ? 'none' : 'block';
+    document.getElementById('menu').classList.toggle('active');
 }
+
+async function updateCounter() {
+    try {
+        const response = await fetch('/api/server-data');
+        const data = await response.json();
+        const memberCount = data.approximate_member_count;
+        document.getElementById('memberCount').textContent = memberCount;
+
+        let seconds = 30;
+        const updateTimeElement = document.getElementById('updateTime');
+
+        setInterval(async () => {
+            seconds--;
+            if (seconds < 0) {
+                seconds = 30;
+                const newResponse = await fetch('/api/server-data');
+                const newData = await newResponse.json();
+                const newMemberCount = newData.approximate_member_count;
+                document.getElementById('memberCount').textContent = newMemberCount;
+            }
+            updateTimeElement.textContent = `00:${seconds.toString().padStart(2, '0')}`;
+        }, 1000);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données du serveur:', error);
+    }
+}
+
+// Initialiser le compteur
+updateCounter();
